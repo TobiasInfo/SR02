cb96d075b681dc4ffd8b7b2f92cf6730a9ee88ca
Apertaro <ma.bureau99@gmail.com>
2023-03-24 19:37:12 +0100

    Version_Finale

--- TD2/Exo3/sig.c
+++ TD2/Exo3/sig.c
@@ -11,6 +11,9 @@
 struct sigaction ActFils;
 struct sigaction ActPere;
 pid_t fils_pid;
+
+//Compteurs de SIGINT
+//Hypothèse : les compteurs de SIGINT sont indépendants 
 int cptPere = 0;
 int cptFils = 0;
 
@@ -19,6 +22,8 @@ int cptFils = 0;
 void captfils(){
     rectvert(5);
     cptFils++;
+    
+    // 3 SIGINT Recu pour le processus fils
     if(cptFils == 3){
         detruitrec();
         exit(0);
@@ -29,54 +34,83 @@ void captfils(){
 //Fonction pere
 void captpere(){
     cptPere++;
-    printf(" PERE %d : signal %d recu\n", getpid(),cptPere);
+    printf("PERE %d : signal %d recu\n", getpid(),cptPere);
     fflush(stdout);
+    
+    //Si 3 SIGINT reçus pour le père
     if(cptPere == 3){
-        printf("  PERE : fin du pere , trois signals sont deja recu\n");
+        printf("PERE : fin du pere , trois signals sont deja recu\n");
         fflush(stdout);
         exit(0);
     }
 }
 
+
+//------------------------------------------------------------------------------------------
+
+//Main
+
 int main(){
     fils_pid = fork();
+    
+    //Partie Processus fils
     if (fils_pid == 0){
+    int i =0;
         initrec();
         ActFils.sa_handler = captfils;
         sigaction(SIGINT, &ActFils, 0);
-        int i = attendreclic();
+        
+        //Tant qu'on ne clic pas sur le boutton de fin
         while (i != -1)
         {
+            i = attendreclic();
+            
+            //On clic sur 0
             if(i==0){
-            printf("FILS envoit au PERE\nFILS : pid de mon pere est : %d\n", getppid());
-            fflush (stdout);
+            	printf("FILS envoit au PERE\nFILS :  pid du mon pere est : %d\n", getppid());
+            	fflush (stdout);
+            
+            //Envoie du SIGINT au père
                 kill(getppid(), SIGINT);
             }
-            printf(" FILS :  pid du mon pere est : %d \n", getppid());
+            
+            //On clic sur autre chose que 0
+            else {
+            printf("FILS :  pid de mon pere est : %d \n", getppid());
             fflush(stdout);
-            i = attendreclic();
+            }
         }
-
         
-    } else if (fils_pid > 0){
+        // Après clic sur fin 
+	printf("FILS :fin du fils apres clic sur FIN\n");
+	fflush(stdout);
+	exit(0);
+    } 
+    
+    // Partie processus père
+    else if (fils_pid > 0){
         ActPere.sa_handler = captpere;
         sigaction(SIGINT, &ActPere, 0);
+        
+        // Attente pour être sur que le sigaction 
+        // du fils a bien été pris en compte
         sleep(1);
-        // kill(fils_pid, SIGINT);
         int n;
         while (1)
         {
             n = sleep(10);
             printf("temps restant : %d \n", n);
             fflush(stdout);
-            //printf("temps restant : %d \n", n);
-            // fflush(stdout);
-            // ecritreceived(n);
         }
-    } else {
+        
+    } 
+    
+    // Si on a pas réussi à créer le fils
+    else {
         printf("Erreur de création du fils");
         fflush(stdout);
     }
     printf("\n");
     fflush(stdout);
+    return 0;
 }
