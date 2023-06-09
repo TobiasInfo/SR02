1625286dbc64011d702d49b3b8500a3d8cfb9993
TobiasInfo <savarytobias@hotmail.com>
2023-06-09 20:08:26 +0200

    version optimise

--- TD8_Thread/Eratostene_parallele_opti2.c
+++ TD8_Thread/Eratostene_parallele_opti2.c
@@ -13,19 +13,42 @@ typedef struct
     pthread_mutex_t *mutex;
 } ArgumentThread;
 
+// void *fonctionThread(void *argument)
+// {
+//     ArgumentThread *arg = (ArgumentThread *)argument;
+//     unsigned long *tab = arg->tab;
+//     unsigned long i = arg->i;
+//     unsigned long n = arg->n;
+//     for (unsigned long t = i + 3; t <= (arg->racine); t += k)
+//     {
+//         printf("%lu\n", t);
+//         if (tab[t-3] == 1)
+//         {
+//             unsigned long l = 2 * t + 3;
+//             pthread_mutex_lock(arg->mutex);
+//             for (unsigned long j = (l * l - 3) / 2; j < n / 2; j += l)
+//             {
+//                 tab[j] = 0;
+//             }
+//             pthread_mutex_unlock(arg->mutex);
+//         }
+//     }
+//     pthread_exit(NULL);
+// }
+
 void *fonctionThread(void *argument)
 {
     ArgumentThread *arg = (ArgumentThread *)argument;
     unsigned long *tab = arg->tab;
     unsigned long i = arg->i;
     unsigned long n = arg->n;
-    for (unsigned long t = i; t <= (arg->racine - 3) / 2; t += k)
+    for (unsigned long t = i; t <= (arg->racine); t += k)
     {
         if (tab[t] == 1)
         {
             unsigned long l = 2 * t + 3;
             pthread_mutex_lock(arg->mutex);
-            for (unsigned long j = (l * l - 3) / 2; j <= n / 2; j += l)
+            for (unsigned long j = (l * l - 3) / 2; j < n / 2 - 1; j += l)
             {
                 tab[j] = 0;
             }
@@ -54,7 +77,7 @@ void Eratosthenes(unsigned long n, unsigned long *tab)
         pthread_create(&thread[indexThread], NULL, fonctionThread, arg);
         indexThread++;
     }
-    for (unsigned long i = 0; i < indexThread; i++)
+    for (unsigned int i = 0; i < indexThread; i++)
     {
         pthread_join(thread[i], NULL);
     }
@@ -66,7 +89,7 @@ void PrintTab(unsigned long n, unsigned long *tab)
 {
     // Affichage des nombres premiers
     printf("2 ");
-    for (unsigned long i = 0; i <= (n / 2) -1; i++)
+    for (unsigned long i = 0; i < (n / 2) -1; i++)
     {
         if (tab[i] == 1)
         {
@@ -80,15 +103,15 @@ void PrintTab(unsigned long n, unsigned long *tab)
 void PrintIndex(unsigned long n, unsigned long *tab)
 {
     int cpt = 1; // Initialisé à 1 pour inclure le nombre premier 2
-    for (unsigned long i = 0; i <= (n / 2) - 1; i++)
+    for (unsigned long i = 0; i < (n / 2) - 1; i++)
     {
-        printf("%ld ", tab[i]);
+        // printf("%ld ", tab[i]);
         if (tab[i] == 1)
         {
             cpt++;
         }
     }
-    printf("%d\n", cpt);
+    printf("CPT : %d\n", cpt);
     printf("\n");
 }
 
@@ -103,11 +126,11 @@ int main(int argc, char *argv[])
 
     if (n > 1)
     {
-        unsigned long *tab = (unsigned long *)malloc((n / 2 + 1) * sizeof(unsigned long));
+        unsigned long *tab = (unsigned long *)malloc(((n / 2) - 1) * sizeof(unsigned long));
         clock_t start, end;
         double temps_execution;
         // Initialisation du tableau
-        for (unsigned long i = 0; i <= (n / 2); i++)
+        for (unsigned long i = 0; i < (n / 2) - 1; i++)
         {
             tab[i] = 1;
         }
